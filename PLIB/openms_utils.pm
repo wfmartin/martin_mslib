729aa57e84b44fa9f8c7f85a76937c493cb8fbbb
William Martin <wfmartin@ucdavis.edu>
2013-06-10 19:47:47 -0700

    syntax change for call to new IO::File

--- PLIB/openms_utils.pm
+++ PLIB/openms_utils.pm
@@ -157,7 +157,7 @@ sub output_peptide_hit {
 sub create_featurexml_file {
   my ($featuremap_name, $outfile_name, $a_rows) = @_;
 
-  my $outf = IO::File->new(">$outfile_name");
+  my $outf = new IO::File(">$outfile_name");
   my $w = XML::Writer->new(OUTPUT=>$outf, DATA_INDENT=>2);
 
   print $outf '<?xml version="1.0" encoding="ISO-8859-1"?>', "\n";
